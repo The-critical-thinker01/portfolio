21e4dd967672e4ada02568c88996977e966f1168
The-critical-thinker01 <boycaro19990@email.com>
2024-03-27 22:27:40 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: " REACT & NODE JS ",
+    subtitle: "QUIZSHOW",
+    description:
+      "QuizShow is an online study platform I developed using React. It provides an interactive and engaging way for users to learn and test their knowledge on various subjects. Check it out here.",
+    image: "./project-1.gif",
+    link: "https://site-ime-bafoussam-ojbz.vercel.app",
+  },
+  {
+    title: "HTML/CSS & JS",
+    subtitle: "Agent Ledor",
+    description:
+      "Agent is a real estate website I created. It allows users to browse property listings, get in touch with real estate agents, and find their dream home. Explore it here.",
+    image: "./project-2.gif",
+    link: "https://ledor-agent.vercel.app",
+  },
+  {
+    title: "Ime-school",
+    subtitle: "HTML/CSS & JS",
+    description:
+      "IME is a website I developed for presenting a school. It provides comprehensive information about the school, its programs, faculty, and more. Visit the site here.",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "HTML/CSS ",
+    subtitle: "CV PROJECT ",
+    description:
+      "This is a personal website I created using HTML, CSS, and JavaScript. It serves as an online CV, showcasing my skills, experiences, and projects. You can view it here.",
+    image: "./project-4.gif",
+    link: "https://cv-ronaldo.vercel.app",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I’ve had the pleasure of working with Ronaldo on several projects. His passion for programming is evident in every line of code he writes. He’s not just a programmer, but a creative problem solver who isn’t afraid to think outside the box. His dedication and work ethic are truly commendable. I look forward to our future collaborations.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Ronaldo’s work is a testament to his love for programming. His attention to detail, innovative thinking, and relentless pursuit of excellence set him apart in this field. He’s not only a talented programmer but also a great team player who’s always ready to help. Working with him has been an absolute joy.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
